95fd3b4ce36a1d3b9af89f6316f880bb0b70079c
Deanosim <4834976+Deanosim@users.noreply.github.com>
2022-05-22 03:26:18 +1000

    Create HardDriveBridge.php

---
+++ bridges/HardDriveBridge.php
@@ -0,0 +1,52 @@
+<?php
+class HardDriveBridge extends FeedExpander {
+
+	const MAINTAINER = 'Deanosim';
+	const NAME = 'Hard-Drive Bridge';
+	const URI = 'https://hard-drive.net/';
+	const DESCRIPTION = 'RSS Feed for Hard-Drive.net';
+	const PARAMETERS = array();
+	const CACHE_TIMEOUT = 10;
+
+    public function collectData(){
+        $this->collectExpandableDatas('https://hard-drive.net/feed/');
+    }
+
+	protected function parseItem($newsItem){
+		$item = parent::parseItem($newsItem);
+
+		// --- Recovering the article ---
+
+		// $articlePage gets the entire page's contents
+		$articlePage = getSimpleHTMLDOM($newsItem->link);
+		// figure contain's the main article image
+		$article = $articlePage->find('div.featured-image', 0);
+		// content__article-body has the actual article
+		foreach($articlePage->find('div.post-content') as $element)
+			$article = $article . $element;
+
+		// --- Fixing ugly elements ---
+
+		// Replace the image viewer and BS with the image itself
+		//foreach($articlePage->find('.attachment-full') as $uslElementLoc) {
+			//$main_img = $uslElementLoc->find('img', 0);
+			//$article = str_replace($uslElementLoc, $main_img, $article);
+		//}
+
+		// List of all the crap in the article
+		$uselessElements = array(
+			'div.post-content script'
+		);
+
+		// Remove the listed crap
+		foreach($uselessElements as $uslElement) {
+			foreach($articlePage->find($uslElement) as $uslElementLoc) {
+				$article = str_replace($uslElementLoc, '', $article);
+			}
+		}
+
+		$item['content'] = $article;
+
+		return $item;
+	}
+}
