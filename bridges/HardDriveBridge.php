267cf68d4e31769b98ff4321353a13850c75bf9d
Deanosim <4834976+Deanosim@users.noreply.github.com>
2022-05-22 04:48:08 +1000

    Update HardDriveBridge.php
    passthrough author

--- bridges/HardDriveBridge.php
+++ bridges/HardDriveBridge.php
@@ -12,8 +12,9 @@ class HardDriveBridge extends FeedExpander {
         $this->collectExpandableDatas('https://hard-drive.net/feed/');
     }
 
-	protected function parseItem($newsItem){
-		$item = parent::parseItem($newsItem);
+		protected function parseItem($newsItem){
+    $item = parent::parseItem($newsItem);
+    $item['categories'] = parent::parseItem($newsItem->tags);
 
 		// --- Recovering the article ---
 
@@ -39,7 +40,14 @@ class HardDriveBridge extends FeedExpander {
 			}
 		}
 
+		$item['uri'];        // URI to reach the subject ("https://...")
+		$item['title'];      // Title of the item
+		$item['timestamp'];  // Timestamp of the item in numeric or text format (compatible for strtotime())
+		$item['author'];     // Name of the author for this item
 		$item['content'] = $article;
+		$item['enclosures']; // Array of URIs to an attachments (pictures, files, etc...)
+		//$item['categories'];
+		$item['uid'];        // A unique ID to identify the current item
 
 		return $item;
 	}
